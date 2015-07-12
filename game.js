cd9ddf039ead517dc89190dc151b0049ccb27640
Eric Flores <eric.t.flores@gmail.com>
2015-07-11 22:54:08 -0700

    add player movement with cursor keys

--- game.js
+++ game.js
@@ -4,27 +4,54 @@ var game = new Phaser.Game(800,600, Phaser.AUTO, 'phaser-demo', {preload: preloa
 
 var player;
 var starfield;
+var cursors;
 
 
 function preload() {
     game.load.image('starfield', './assets/starfield.png');
     game.load.image('ship', './assets/player.png');
-}
+}// ends the preload function
 
 function create() {
   //  The scrolling starfield background
     starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
     //  The hero!
     player = game.add.sprite(400, 500, 'ship');
+    player.anchor.setTo(0.5, 0.5);
+    game.physics.enable(player, Phaser.Physics.ARCADE);//this gives us access to the arcade physics engine so we can add physics to our game
 
-}
+    // And some controls to play the game with
+    cursors = game.input.keyboard.createCursorKeys();
+
+
+}//ends the create function
 
 function update() {
   //scroll the background
-  starfield.tilePosition.y += 3
+  starfield.tilePosition.y += 3;
 
-}
+  // reset the player, then check for movement keys
+  player.body.velocity.setTo(0, 0);
+
+  if (cursors.left.isDown)
+  {
+    player.body.velocity.x = -200;
+  }
+  else if (cursors.right.isDown)
+  {
+    player.body.velocity.x = 200
+  }
+  else if (cursors.up.isDown)
+  {
+    player.body.velocity.y = -200;
+  }
+  else if (cursors.down.isDown)
+  {
+    player.body.velocity.y = 200;
+  }
+
+}//ends the update function
 
 function render() {
 
-}
+}//ends the render function
